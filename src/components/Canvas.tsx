cdc993cb72c233de15aa8c9716fec44acd8ce999
Wanderson Silva <wandersonsilva768@gmail.com>
2021-07-17 20:19:45 -0300

    feat: commit inicial de arquivos

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Canvas () {
+  return (
+    <div className="canvas">
+      <div className="canvas__wrapper">
+        <canvas id="draw_canvas">
+          
+        </canvas>
+      </div>
+    </div>
+  );
+}
+
+export default Canvas;
---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Canvas } from '../components';
+
+function App () {
+  return (
+    <div className="App">
+      hello
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
